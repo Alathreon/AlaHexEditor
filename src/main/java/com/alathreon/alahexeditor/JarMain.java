c556de159ee9ebc348922872629cfc74b47d6264
Alathreon <master64046@gmail.com>
2025-10-18 17:37:45 +0200

    Using exec plugin instead of javafx plugin

--- src/main/java/com/alathreon/alahexeditor/JarMain.java
+++
@@ -1,9 +0,0 @@
-package com.alathreon.alahexeditor;
-
-import javafx.application.Application;
-
-public class JarMain {
-    public static void main(String[] args) {
-        Application.launch(App.class, args);
-    }
-}
---
+++ src/main/java/com/alathreon/alahexeditor/Main.java
@@ -0,0 +1,9 @@
+package com.alathreon.alahexeditor;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
+    }
+}
